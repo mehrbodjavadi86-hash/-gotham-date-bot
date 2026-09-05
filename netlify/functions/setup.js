ae06e513f712ba115eb65db5fd213944242492d8
mehrbodjavadi86-hash <mehrbodjavadi86@gmail.com>
2026-09-06 03:05:33 +0330

    Update setup.js

--- netlify/functions/setup.js
+++ netlify/functions/setup.js
@@ -1,34 +1,5 @@
 export default async () => {
-  const token = process.env.BOT_TOKEN;
-
-  if (!token) {
-    return new Response("BOT_TOKEN پیدا نشد", {
-      status: 500
-    });
-  }
-
-  const webhookUrl =
-    "https://gotham-date.netlify.app/.netlify/functions/telegram";
-
-  const response = await fetch(
-    `https://api.telegram.org/bot${token}/setWebhook`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        url: webhookUrl
-      })
-    }
-  );
-
-  const result = await response.text();
-
-  return new Response(result, {
-    status: response.status,
-    headers: {
-      "Content-Type": "application/json"
-    }
+  return new Response("SETUP FUNCTION WORKS!", {
+    status: 200
   });
 };
